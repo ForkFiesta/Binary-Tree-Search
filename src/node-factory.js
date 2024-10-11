5621a0026f1219a684cc0ce8d06c7476c0d865c4
Raul Resendez <raul.resendez@proton.me>
2024-10-11 00:35:28 -0500

    Create node class.

---
+++ src/node-factory.js
@@ -0,0 +1,7 @@
+export class Node {
+  constructor(data, leftChildren, rightChildren) {
+    this.data = data;
+    this.leftChildren = leftChildren;
+    this.rightChildren = rightChildren;
+  }
+}
